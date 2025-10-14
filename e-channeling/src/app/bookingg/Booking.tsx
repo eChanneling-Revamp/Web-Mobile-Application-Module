2bdf4a022912d24ee0b8982da706f5f40bcccc8a
kasuni dulanjaly <kasunidulanjaly@gmail.com>
2025-10-14 20:49:54 +0530

    chore: add types and imports for booking slice

---
+++ e-channeling/src/app/bookingg/Booking.tsx
@@ -0,0 +1,13 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+/* ---------- Types ---------- */
+export type Step = 1 | 2 | 3 | 4;
+
+interface PatientDetails {
+  fullName: string;
+  phone: string;
+  email: string;
+  age: string;
+  gender: string;
+  reason: string;
+}
