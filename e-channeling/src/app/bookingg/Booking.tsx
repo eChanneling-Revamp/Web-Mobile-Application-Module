e7641d53e2d28df4e303aa7b699e8a54f824ff3a
kasuni dulanjaly <kasunidulanjaly@gmail.com>
2025-10-15 15:10:45 +0530

    refactor: rename bookingg folder to booking for consistent naming

--- e-channeling/src/app/bookingg/Booking.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "@/store";
-import { setStep, setPatientDetails, resetBooking } from "@/store/booking/bookingSlice";
-
